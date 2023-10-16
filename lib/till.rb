bb0dca9a70332eb6cad1cc20ae4f5775bd9c495b
develaper <ibrahim.lachguer@gmail.com>
2023-10-16 20:09:39 +0200

    Add offers to till

--- lib/till.rb
+++ lib/till.rb
@@ -1,10 +1,27 @@
 require './lib/shop'
+require './lib/offer'
 
 class Till
   # Params: Array of Strings
   # Returns Float
   #
   def self.total_cost(items)
+    (subtotal_cost(items) + total_discount(items)).round(2)
+  end
+
+  # Params: Array of Strings
+  # Returns Float
+  #
+  def self.subtotal_cost(items)
     items.map{|item| Shop.item_cost(item)}.sum
+  end
+  
+  # Params: Array of Strings
+  # Returns Float
+  #
+  def self.total_discount(items)
+    Offer.buy_x_get_y(items, 2, 1, "GR1") +
+    Offer.buy_x_get_y_fraction_discount(items, 3, 10, "SR1") +
+    Offer.buy_x_get_y_fraction_discount(items, 3, 3, "CF1")
   end
 end
--- test/till_test.rb
+++ test/till_test.rb
@@ -2,8 +2,28 @@ require 'minitest/autorun'
 require './lib/till'
 
 class TestTill < Minitest::Test
-  def test_total_cost_with_example_list
+  def test_total_cost_with_example_list_without_discounts
     items = ["GR1", "SR1", "CF1"]
     assert_equal 19.34, Till.total_cost(items)
+  end
+  def test_total_cost_with_green_tea_discount
+    items = ["GR1", "GR1"]
+    assert_equal 3.11, Till.total_cost(items)
+  end
+  def test_total_cost_with_green_tea_discount_only_for_pairs
+    items = ["GR1", "GR1", "GR1"]
+    assert_equal 6.22, Till.total_cost(items)
+  end
+  def test_total_cost_with_strawberries_discount
+    items = ["SR1", "SR1", "GR1", "SR1"]
+    assert_equal 16.61, Till.total_cost(items)
+  end
+  def test_total_cost_with_coffee_discount
+    items = ["GR1", "CF1", "SR1", "CF1", "CF1"]
+    assert_equal 30.57, Till.total_cost(items)
+  end
+  def test_total_cost_with_coffee_discount_and_green_tea_discount
+    items = ["GR1", "CF1", "SR1", "CF1", "CF1", "GR1"]
+    assert_equal 30.57, Till.total_cost(items)
   end
 end
