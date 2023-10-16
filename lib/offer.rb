9780e633c215e6d20af44c4b9ea124d6b67ff0fc
develaper <ibrahim.lachguer@gmail.com>
2023-10-16 13:35:24 +0200

    Added price should drop to 2/3 of the original price offer

--- lib/offer.rb
+++ lib/offer.rb
@@ -4,9 +4,9 @@ class Offer
   def self.buy_x_get_y(items, x, y, item)
     (items.count(item) / x) * (x - y) * -Shop.item_cost(item)
   end
-  def self.buy_x_get_y_percent(items, x, y, item)
+  def self.buy_x_get_y_fraction_discount(items, x, y, item)
     if items.count(item) >= x
-       discount = (-Shop.item_cost(item).to_f / 10) * items.count(item)
+       discount = (-Shop.item_cost(item).to_f / y) * items.count(item)
     end
   end
 end
--- test/offer_test.rb
+++ test/offer_test.rb
@@ -10,12 +10,20 @@ class TestOffer < Minitest::Test
     items = ["GR1", "SR1", "CF1", "GR1"]
     assert_equal -3.11, Offer.buy_x_get_y(items, 2, 1, "GR1")
   end
-  def test_buy_x_get_y_percent_with_10_for_3_strawberries_with_minimal_list
+  def test_buy_x_get_y_fraction_discount_with_10_for_3_strawberries_with_minimal_list
     items = ["SR1", "SR1", "SR1"]
-    assert_equal -1.50, Offer.buy_x_get_y_percentage(items, 3, 10, "SR1")
+    assert_equal -1.50, Offer.buy_x_get_y_fraction_discount(items, 3, 10, "SR1")
   end
-  def test_buy_x_get_y_percent_with_10_for_3_strawberries_with_more_complex_list
+  def test_buy_x_get_y_fraction_discount_with_10_for_3_strawberries_with_more_complex_list
     items = ["SR1", "SR1","GR1", "SR1", "CF1"]
-    assert_equal -1.50, Offer.buy_x_get_y_percent(items, 3, 10, "SR1")
+    assert_equal -1.50, Offer.buy_x_get_y_fraction_discount(items, 3, 10, "SR1")
+  end
+  def test_buy_x_get_y_fraction_discount_with_3_for_3_coffees_with_minimal_list
+    items = ["CF1", "CF1", "CF1"]
+    assert_equal -11.23, Offer.buy_x_get_y_fraction_discount(items, 3, 3, "CF1")
+  end
+  def test_buy_x_get_y_fraction_discount_with_3_for_3_coffees_with_more_complex_list
+    items = ["CF1", "SR1","GR1", "CF1", "CF1"]
+    assert_equal -11.23, Offer.buy_x_get_y_fraction_discount(items, 3, 3, "CF1")
   end
 end
