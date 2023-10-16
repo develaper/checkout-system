9527e11c42194815975b55d38cb0fdeac14fb8d2
develaper <ibrahim.lachguer@gmail.com>
2023-10-16 13:24:22 +0200

    Added price discount for bulk purchases offer

--- lib/offer.rb
+++ lib/offer.rb
@@ -1,5 +1,12 @@
+require './lib/shop'
+
 class Offer
   def self.buy_x_get_y(items, x, y, item)
     (items.count(item) / x) * (x - y) * -Shop.item_cost(item)
+  end
+  def self.buy_x_get_y_percent(items, x, y, item)
+    if items.count(item) >= x
+       discount = (-Shop.item_cost(item).to_f / 10) * items.count(item)
+    end
   end
 end
--- test/offer_test.rb
+++ test/offer_test.rb
@@ -3,11 +3,19 @@ require './lib/offer'
 
 class TestOffer < Minitest::Test
   def test_buy_x_get_y_with_2_for_1_green_tea_with_minimal_list
-    items = ["GR1", "GR1"]
+    items = ["GR1", "GR1", "GR1"]
     assert_equal -3.11, Offer.buy_x_get_y(items, 2, 1, "GR1")
   end
   def test_buy_x_get_y_with_2_for_1_green_tea_with_more_complex_list
     items = ["GR1", "SR1", "CF1", "GR1"]
     assert_equal -3.11, Offer.buy_x_get_y(items, 2, 1, "GR1")
+  end
+  def test_buy_x_get_y_percent_with_10_for_3_strawberries_with_minimal_list
+    items = ["SR1", "SR1", "SR1"]
+    assert_equal -1.50, Offer.buy_x_get_y_percentage(items, 3, 10, "SR1")
+  end
+  def test_buy_x_get_y_percent_with_10_for_3_strawberries_with_more_complex_list
+    items = ["SR1", "SR1","GR1", "SR1", "CF1"]
+    assert_equal -1.50, Offer.buy_x_get_y_percent(items, 3, 10, "SR1")
   end
 end
